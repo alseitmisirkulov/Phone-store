22377bf70ab98ec79100a94ec7d5090d0d6f4668
azat <arykov.azat.kg@gmail.com>
2023-04-08 16:03:59 +0600

    add: main content cases headphones and airpods footer

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import iPhoneSvg from './../../assets/images/iPhone-13-Pro-Max.svg';
-import './About.scss'
+import './About.scss';
 
 export const About = () => {
   return (
     <section className="about container d-flex ">
-      <div className='about__description'>
+      <div className="about__description">
         <h1>Аксессуары для Iphone 13 Pro Max</h1>
       </div>
-      <figure>
-        <img src={iPhoneSvg} alt="" />
-      </figure>
+      <figure></figure>
     </section>
   );
 };
