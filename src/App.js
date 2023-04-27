5e6e619ed2eb09bb134d49c3ff47e6dec08e59ee
azat <arykov.azat.kg@gmail.com>
2023-04-27 18:03:52 +0600

    add: loader

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import { NotFound } from './pages/NotFound';
 import { CatalogItem } from './pages/CatalogItem';
 import { useState, useEffect } from 'react';
 import { dataBase } from './data/db';
+import { Loader } from './components/common/select/loader/Loader';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
   const [db, setDb] = useState(dataBase);
   const [emptyCartDb, setEmptyCartDb] = useState([]);
   const [favorite, setFavorite] = useState([]);
@@ -64,44 +67,53 @@ function App() {
     setFavorite(JSON.parse(localStorage.getItem('favoriteStore')) || []);
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  }, []);
   return (
-    <div className="App ">
-      <Header totalQty={totalQty} favorite={favorite} />
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage db={db} favorite={favorite} setFavorite={setFavorite} />}
-        />
-        <Route
-          path="/cart"
-          element={
-            <Cart
-              emptyCartDb={emptyCartDb}
-              handleRemove={handleRemove}
-              handleAddToCart={handleAddToCart}
-              handleMinusItem={handleMinusItem}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="App ">
+          <Header totalQty={totalQty} favorite={favorite} />
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage db={db} favorite={favorite} setFavorite={setFavorite} />}
             />
-          }
-        />
-        <Route
-          path="/favorites"
-          element={
-            <Favorites
-              favorite={favorite}
-              setFavorite={setFavorite}
-              handleRemoveFav={handleRemoveFav}
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  emptyCartDb={emptyCartDb}
+                  handleRemove={handleRemove}
+                  handleAddToCart={handleAddToCart}
+                  handleMinusItem={handleMinusItem}
+                />
+              }
             />
-          }
-        />
-        <Route path="/service" element={<Service />} />
-        <Route
-          path="/catalog-item/:id"
-          element={<CatalogItem handleAddToCart={handleAddToCart} db={db} />}
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+            <Route
+              path="/favorites"
+              element={
+                <Favorites
+                  favorite={favorite}
+                  setFavorite={setFavorite}
+                  handleRemoveFav={handleRemoveFav}
+                />
+              }
+            />
+            <Route path="/service" element={<Service />} />
+            <Route
+              path="/catalog-item/:id"
+              element={<CatalogItem handleAddToCart={handleAddToCart} db={db} />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import { About } from '../components/about/About';
 import { MainContent } from './../components/mainContent/MainContent';
 
 export const HomePage = ({ db, favorite, setFavorite }) => {
+  
+
   return (
     <div>
       <About />
-      <MainContent db={db} favorite={favorite} setFavorite={setFavorite}/>
+      <MainContent db={db} favorite={favorite} setFavorite={setFavorite} />
     </div>
   );
 };
